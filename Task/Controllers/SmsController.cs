68879cd56483269848bd332232efbfb12c98e3ea
mohamed mosaad <103392388+mohamedmosaadmohed@users.noreply.github.com>
2023-07-12 18:39:46 +0300

    Add files via upload

---
+++ Task/Controllers/SmsController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task.Models;
+using System.Text.RegularExpressions;
+
+
+namespace Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmsController : ControllerBase
+    {
+        private readonly SmsMisrProperties _smsConfig;
+
+        public SmsController()
+        {
+            _smsConfig = new SmsMisrProperties(
+                "ed810713d5566299555e961265f91315e5ec169924727e38eeac06b2fb736b13",
+                "b611afb996655a94c8e942a823f1421de42bf8335d24ba1f84c437b2ab11ca27",
+                "c96c511a226d104e40251607398596bc231ffcce63207b67861ef1e94672642e");
+        }
+        [HttpPost]
+        public async Task<ActionResult> SendSmsAsync(List<string> mobileNumbers, string Message, string language, decimal MaxCost, DateTime sendDate)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Message))
+                {
+                    return BadRequest("Message cannot be empty");
+                }
+
+                if (MaxCost <= 0)
+                {
+                    return BadRequest("Invalid MaxCost");
+                }
+
+                string dateTime = "";
+                if (sendDate != default(DateTime))
+                {
+                    dateTime = sendDate.ToString("yyyy-MM-ddTHH:mm:ssZ"); // 2023-07-05T10:00:00.00Z
+                }
+                else
+                {
+                    sendDate = DateTime.Now;
+                }
+
+                int lang = language.ToLower() == "english" || language.ToLower() == "en" ? 1 : 2;
+                decimal MaxNumberForOneCost = lang == 2 ? 70m : 160m;
+                decimal MaxCharacters = MaxCost * MaxNumberForOneCost;
+                decimal Cost = (mobileNumbers.Count * Message.Length);
+                decimal CostForMessage = Cost / MaxNumberForOneCost;
+                if (MaxCharacters <= Cost)
+                {
+                    return BadRequest($"The cost is {CostForMessage} More than MaxCost that You Enter");
+                }
+
+                Regex regex = new Regex(@"^(?:\+2)?01[0125]\d{8}$");
+                foreach (string number in mobileNumbers)
+                {
+                    if (!regex.IsMatch(number))
+                    {
+                        return BadRequest("Invalid phone number");
+                    }
+                }
+
+                var encodedMessage = Uri.EscapeDataString(Message);
+                var url = $"https://smsmisr.com/api/SMS/?environment=2&username={_smsConfig.Username}&password={_smsConfig.Password}&sender={_smsConfig.SenderID}&mobile={string.Join(",", mobileNumbers)}&language={lang}&message={encodedMessage}&DelayUntil={dateTime}";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.PostAsync(url, null);
+                    response.EnsureSuccessStatusCode();
+                    var responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Handle the response from the SMS provider's API
+                    if (!string.IsNullOrEmpty(responseContent))
+                    {
+                        // stored successfull sms in database
+                        var mobileNumbersString = string.Join(",", mobileNumbers);
+                        var sms = new SuccessfullyMessage(mobileNumbersString, Message, language, CostForMessage, sendDate);
+                        await sms.InsertIntoDatabaseAsync();
+                        return Ok($"SMS send and stored Successfully and Take {CostForMessage}");
+                    }
+                    else
+                    {
+                        return BadRequest("SMS failed to send");
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error sending SMS: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpPost("OTP")]
+        public async Task<ActionResult> SendOTPAsync(string mobileNumber)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?:\+2)?01[0125]\d{8}$");
+                if (regex.IsMatch(mobileNumber))
+                {
+                    // Generate a random 6-digit OTP
+                    Random random = new Random();
+                    int otpValue = random.Next(100000, 999999);
+
+                    // Set the SMS template token and URL using the generated OTP
+                    string templateToken = "0f9217c9d760c1c0ed47b8afb5425708da7d98729016a8accfc14f9cc8d1ba83";
+                    string url = $"https://smsmisr.com/api/OTP/?environment=2&username={_smsConfig.Username}&password={_smsConfig.Password}&sender={_smsConfig.SenderID}&mobile={mobileNumber}&template={templateToken}&otp={otpValue}";
+
+                    HttpClient client = new HttpClient();
+                    HttpResponseMessage response = await client.PostAsync(url, null);
+                    response.EnsureSuccessStatusCode();
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    if (responseContent != null)
+                    {
+                        return Ok($"OTP sent successfully to {mobileNumber}");
+                    }
+                    else
+                    {
+                        return BadRequest($"Failed to send OTP to {mobileNumber}");
+                    }
+                }
+                return Ok();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error sending OTP message: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ Task/Models/successfullyMessage.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using SOfCO.Helpers;
+
+namespace Task.Models
+{
+    public class SuccessfullyMessage
+    {
+            private readonly SQL _sql;
+
+            public int Id { get; set; }
+            public string MobileNumbers { get; set; }
+            public string Message { get; set; }
+            public string Language { get; set; }
+            public decimal Cost { get; set; }
+            public DateTime SentDateTime { get; set; }
+        public SuccessfullyMessage(string mobileNumbers, string message, string language, decimal cost, DateTime sentDateTime)
+        {
+            MobileNumbers = string.Join(",", mobileNumbers);
+            Message = message;
+            Language = language;
+            Cost = cost;
+            SentDateTime = sentDateTime;
+            string conection = "Data Source=MOHAMED;Initial Catalog=SMS;Integrated Security=True";
+            _sql = new SQL(conection);
+        }
+
+        public async ValueTask InsertIntoDatabaseAsync()
+        {
+            // Call the stored procedure to insert the SMS message into the database
+            var cmd = new SqlCommand("InsertSmsMessage");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MobileNumbers", MobileNumbers);
+            cmd.Parameters.AddWithValue("@Message", Message);
+            cmd.Parameters.AddWithValue("@Language", Language);
+            cmd.Parameters.AddWithValue("@Cost", Cost);
+            cmd.Parameters.AddWithValue("@SentDateTime", SentDateTime);
+            await _sql.ExecuteNonQueryAsync(cmd);
+        }
+    }
+}
